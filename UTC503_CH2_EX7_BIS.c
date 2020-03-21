0db5f7f875010d96431ac627d94fc8b4cc8518b6
Ludo <hluthwigg@gmail.com>
2020-03-22 00:19:48 +0100

    Update UTC503_CH2_EX7_BIS.c

--- UTC503_CH2_EX7_BIS.c
+++ UTC503_CH2_EX7_BIS.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <math.h>
 #define N 5
 
 // Factorisation du code avec un "callback"
 
-int multiplier(int e, int option)
+int multiply(int e, int option)
 {
     int r;
     return r = e * option;
 }
 
-int square(int e)
+int power(int e, int option)
 {
     int r;
-    return r = e * e;
+    return r = pow(e,option);
 }
 
 int maximize(int e, int option)
@@ -44,16 +45,12 @@ int main()
 {
     int e[5] = {1, 5, 2, 4, 3};
     int r_double[N];
-    int option = 2;
-    operation(e, r_double, &multiplier, option); // r_double contiendra {2, 10, 4, 8, 6}
+    operation(e, r_double, &multiply, 2); // r_double contiendra {2, 10, 4, 8, 6}
     int r_triple[N];
-    option = 3;
-    operation(e, r_triple, &multiplier, option); // r_triple contiendra {3, 15, 6, 12, 9}
+    operation(e, r_triple, &multiply, 3); // r_triple contiendra {3, 15, 6, 12, 9}
     int r_square[N];
-    option = 2;
-    operation(e, r_square, &square, option); // r_square contiendra {1, 25, 4, 16, 9}
+    operation(e, r_square, &power, 2); // r_square contiendra {1, 25, 4, 16, 9}
     int r_maximize3[N];
-    option = 3;
-    operation(e, r_maximize3, &maximize, option); // r_maximize3 contiendra {1, 3, 2, 3, 3}
+    operation(e, r_maximize3, &maximize, 3); // r_maximize3 contiendra {1, 3, 2, 3, 3}
     return (0);
 }
